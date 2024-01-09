cd2d9e751d7a97f670f2895b42a538d15aaa57ae
shamilmelis <akastramormoley1st@mail.ru>
2024-01-09 13:15:30 +0600

    upd:3

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Footer/index.scss'
+import '../Footer/media.scss'
+const Footer = () => {
+    return (
+        <footer>
+            <div className={'footer_container'}>
+                <div className={'footer_box'}>
+                    <span className={'copyrights_text'}>Copyright© 2024 Shamil Melisov. All rights reserved.</span>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
